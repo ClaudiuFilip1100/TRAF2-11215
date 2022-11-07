38ddcfac604e07ae043575bbf5ba1aefb5632fe3
ClaudiuFilip1100 <azureuser@TudorMLTest1.tul1txsj5pxulhyaox0iddd23e.fx.internal.cloudapp.net>
2022-11-07 17:53:51 +0000

    add onnx converter

--- scripts/convert_to_onnx.py
+++ scripts/convert_to_onnx.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 import keras
 
-model = keras.models.load_model("../models/tensorflow/NER_model_updated.h5")
+model = keras.models.load_model("../models/tensorflow/NER_model_updated_v2.h5")
 
-tf.saved_model.save(model, "../models/tensorflow/tf_pb_model")
+tf.saved_model.save(model, "../models/tensorflow/tf_pb_model_v2")
 
 import os
 
 os.system(
-    'python -m tf2onnx.convert --saved-model ../models/tensorflow/tf_pb_model --output "../models/onnx/model.onnx"'
+    'python -m tf2onnx.convert --saved-model ../models/tensorflow/tf_pb_model_v2 --output "../models/onnx/model_v2.onnx"'
 )
