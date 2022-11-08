99cc15685da17c1dd57db13cca02207291fc549b
Claudiu Filip <claudiufilip@Claudius-MacBook-Pro.local>
2022-11-08 16:01:55 +0200

    add improved dataset

--- scripts/labeled_pipeline/2_append_csv.py
+++ scripts/labeled_pipeline/2_append_csv.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
-df_1 = pd.read_csv('../../data/dataset_from_json.csv')
-df_2 = pd.read_csv('../../data/dataset_from_json_v2.csv')
+df_1 = pd.read_csv("../../data/dataset_from_json_smaller_vocab.csv")
+df_2 = pd.read_csv("../../data/dataset_from_json_smaller_vocab_v2.csv")
 
 # change the sentence number of df_2
-n_sentences = len(df_1.groupby(df_1['sentence #']).count())
-n_sentences_append = len(df_2.groupby(df_2['sentence #']).count())
+n_sentences = len(df_1.groupby(df_1["sentence #"]).count())
+n_sentences_append = len(df_2.groupby(df_2["sentence #"]).count())
 
-#old_indices
+# old_indices
 old_indices = list(range(n_sentences_append))
 # new_sentences
-new_indices = list(range(n_sentences, n_sentences+n_sentences_append))
+new_indices = list(range(n_sentences, n_sentences + n_sentences_append))
 
 df_2.replace(old_indices, new_indices, inplace=True)
 
 df = df_1.append(df_2)
-df.to_csv('../../data/dataset_v2.csv')
+df.to_csv("../../data/dataset__smaller_v2.csv")
